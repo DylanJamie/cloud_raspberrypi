245afc1f12370b861929be8f2d2e311e6a247ac2
DylanJamie <dylanjjboles@gmail.com>
2026-07-21 23:55:04 -0400

    Planning

---
+++ app/endpoints.py
@@ -0,0 +1,35 @@
+# endpoints.py
+
+# imports
+
+# Create an account
+# POST Method
+# /register
+
+# authentication for users and set the session cookies
+# POST Method
+# /login
+
+# clear the session
+# POST Method
+# /logout
+
+# list files for logged in user
+# GET Method
+# /files
+
+# download a file
+# GET method
+# /files/{file_id}/download
+
+# Upload a file
+# POST Method
+# /files/upload
+
+# delete a file
+# DELETE Method
+# /files/delete
+
+# confirm server is alive
+# /GET Method
+# /health
