1d4420e4b482a38d06590b1fd3278fedb090a36c
DylanJamie <dylanjjboles@gmail.com>
2026-07-25 01:56:29 -0400

    html and css added

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 # imports
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from app.api import endpoints
 from app.database.database import init_db
 
@@ -15,7 +16,10 @@ def on_startup():
     init_db()
 
 app.include_router(endpoints.router)
-    
+
+# Makes anything in app/static/ reachable at /static/...
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 # Endpoint for basic message
 @app.get("/")
 async def root():
